23caac03f33c83e4af30774452ed8bd2df18feb1
Вадим <stalxrd@mail.ru>
2024-02-14 13:28:52 +0700

    episode7

---
+++ bin/brain-gcd.js
@@ -0,0 +1,48 @@
+import readlineSynk from "readline-sync";
+// eslint-disable-next-line import/extensions
+import { userName, name } from '../src/cli.js';
+
+function gcd(a, b) {
+  while (b !== 0) {
+    const temp = b;
+    // eslint-disable-next-line no-param-reassign
+    b = a % b;
+    // eslint-disable-next-line no-param-reassign
+    a = temp;
+  }
+  return a;
+}
+
+function playGameGcd() {
+  userName();
+  console.log("Find the greatest common divisor of given numbers.");
+
+  let correctAnswers = 0;
+  const numQuestions = 3;
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < numQuestions; i++) {
+    const a = Math.floor(Math.random() * 100) + 1;
+    const b = Math.floor(Math.random() * 100) + 1;
+
+    console.log(`Question: ${a} ${b}`);
+    // eslint-disable-next-line no-alert
+    const userAnswer = parseInt(readlineSynk.question('Your answer: ').toLowerCase(), 10);
+
+    if (userAnswer === gcd(a, b)) {
+      console.log("Correct!");
+      // eslint-disable-next-line no-plusplus
+      correctAnswers++;
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${gcd(a, b)}'.`);
+      console.log(`Let's try again, ${name}!`);
+      break;
+    }
+  }
+
+  if (correctAnswers === numQuestions) {
+    console.log(`Congratulations, ${name}!`);
+  }
+}
+
+playGameGcd();
--- src/cli.js
+++ src/cli.js
@@ -5,7 +5,7 @@ let name;
 // eslint-disable-next-line import/prefer-default-export
 export function userName() {
   // eslint-disable-next-line no-shadow
-  name = readlineSynk.question('May i have your name?');
+  name = readlineSynk.question('May i have your name? ');
   console.log(`Hello,  ${name}!`);
 }
 export { name };
