5ea206d669d9e25f41e291e882f2c2cb5bd1c47d
Вадим <stalxrd@mail.ru>
2024-02-28 14:00:08 +0700

    finalcommit2

--- src/cli.js
+++ src/cli.js
@@ -8,5 +8,5 @@ export const userName = () => {
   console.log('Welcome to the Brain Games!');
   name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
-}
+};
 export { name };
